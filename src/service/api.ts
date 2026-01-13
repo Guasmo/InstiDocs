3903c47c89c18a68f524e00469390d6ec03cac6a
guasmo <juancalveti4@gmail.com>
2026-01-13 17:48:13 -0500

    feat: initialize axios instance (src/service/api.ts)

---
+++ src/service/api.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+    baseURL: apiUrl,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get("accessToken"); // localStorage.getItem('accessToken');
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
