6731cb3b7e980cc4f98ce14b2e7b39eb255ecf97
guasmo <juancalveti4@gmail.com>
2026-01-13 17:48:12 -0500

    feat: define api endpoints constants (src/constants/endpoints.ts)

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,11 @@
+export const loginApi = "/auth/login";        // POST
+export const registerApi = "/auth/register";  // POST
+
+export const createUserApi = "/user";          // POST
+export const getUsersApi = "/user";            // GET
+export const getUserByIdApi = "/user";         // GET    /:id
+export const updateUserApi = "/user";          // PATCH  /:id
+export const deleteUserApi = "/user";          // DELETE /:id
+
+export const uploadImageApi = "/upload/image";       // POST
+export const deleteImageApi = "/upload/image";       // DELETE /:filename
